6df9a800a1dafe883667a16f128d5f0728d5c6b5
Jake <Jake@jake-tablet.corp.riotinto.org>
2012-03-19 11:44:01 +0800

    Removing uninstall for now..

--- src/MSTestContrib.UITesting.Quickstart.Wpf/Install.ps1
+++ src/MSTestContrib.UITesting.Quickstart.Wpf/Install.ps1
@@ -1,4 +1,2 @@
 ﻿#$project documentation @ http://msdn.microsoft.com/en-us/library/51h9a6ew(v=VS.100).aspx
 param($installPath, $toolsPath, $package, $project)
-
-Uninstall-Package MSTestContrib.UITesting.Quickstart.Wpf
